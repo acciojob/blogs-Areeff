f3c6dd614443a388d9b58aed5b0c1da9e2a5357d
Areeff <p.areeff786@gmail.com>
2023-06-27 12:07:06 +0530

    done

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,28 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-
+        Blog blog=blogRepository2.findById(blogId).get();
+        Image image=new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        image.setBlog(blog);
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+         imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-
+      Image image=imageRepository2.findById(id).get();
+      String imageDimensions=image.getDimensions();
+      String [] givenImageArea=imageDimensions.split("X");
+      String [] totalArea=screenDimensions.split("X");
+      int l=Integer.parseInt(totalArea[0])/Integer.parseInt(givenImageArea[0]);
+      int b=Integer.parseInt(totalArea[1])/Integer.parseInt(givenImageArea[1]);
+      return l*b;
     }
 }
